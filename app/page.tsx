4bb729db964fad3d5d757fd8ab74cccf108721a1
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-05 10:30:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        
-      Site disconnected due to nonpayment!!🧐
+      <p className="text-2xl font-bold ">Site disconnected due to nonpayment!!🧐</p>
       <br /> <br />
       <p className=" flex flex-auto justify-center text-5xl font-extrabold text-[#58400f]">
         KINDLY PAY YOUR CREDIT TO KEEP THIS SITE ON
