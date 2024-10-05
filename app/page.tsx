6fe1ba017022868e2914dfa072f07a068f1beae2
brooklyn-rgb <brooklynstore151@gmail.com>
2024-10-05 09:31:15 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       
+      Site disconnected due to nonpayment!!
+      <br /> <br />
+      <p className=" flex flex-auto justify-center text-5xl font-extrabold text-[#58400f]">
+        KINDLY PAY YOUR CREDIT TO KEEP THIS SITE ON
+        <br /> THANKS FROM MACSOFT ......!
+      </p>
+      <br /> <br />
+
+      </main>
+      
+    </div>
+  );
+}
